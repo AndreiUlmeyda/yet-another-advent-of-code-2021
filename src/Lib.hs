d36a20fc8a59f7bb8a41457ee7ec451dd00ecbca
Adrian Schurz <adrian.schurz@check24.de>
2021-12-12 20:48:21 +0100

    Work with another type containing the aim in order to solve day 2 part 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,19 +4,29 @@ module Lib
     solutionDay2Part1,
     solutionDay2Part2,
     computeAim,
-    SubmarineMovement (MkSubmarineMovement),
+    SubMovement (MkSubMovement),
+    SubMovementPlus (MkSubMovementPlus),
+    SubDirection (Forward, Down, Up),
+    toSubMovementPlus,
   )
 where
 
-import Control.Exception (MaskingState (MaskedUninterruptible))
 import Data.Bifunctor (bimap)
 
-data SubmarineMovement = MkSubmarineMovement
+data SubMovement = MkSubMovement
   { xDirection :: Int,
-    yDirection :: Int,
+    yDirection :: Int
+  }
+  deriving (Show, Eq)
+
+data SubMovementPlus = MkSubMovementPlus
+  { direction :: SubDirection,
+    magnitude :: Int,
     aim :: Int
   }
   deriving (Show, Eq)
+
+data SubDirection = Forward | Up | Down deriving (Show, Eq)
 
 solutionDay1Part1 :: [String] -> Int
 solutionDay1Part1 = countIncreases . map toInt
@@ -25,39 +35,55 @@ solutionDay1Part2 :: [String] -> Int
 solutionDay1Part2 = countIncreases . slidingWindowSumLengthThree . map toInt
 
 solutionDay2Part1 :: [String] -> Int
-solutionDay2Part1 = multiplyDirections . sumDistances . map (toDirections . words)
+solutionDay2Part1 = multiplyDirections . sumDistances . map (toSubMovement . words)
 
-multiplyDirections :: SubmarineMovement -> Int
-multiplyDirections (MkSubmarineMovement x y aim) = x * y
+multiplyDirections :: SubMovement -> Int
+multiplyDirections (MkSubMovement x y) = x * y
 
-solutionDay2Part2 :: [String] -> Int
-solutionDay2Part2 = multiplyDirections . sumDistancesConsideringAim . map (toDirections . words)
+solutionDay2Part2 :: [String] -> [SubMovementPlus]
+solutionDay2Part2 = computeAim . map (toSubMovementPlus . words)
 
-computeAim :: SubmarineMovement -> SubmarineMovement
-computeAim (MkSubmarineMovement x y aim)
-  | y > 0 = MkSubmarineMovement x y (aim + 5)
-  | y < 0 = MkSubmarineMovement x y (aim - 3)
-  | otherwise = MkSubmarineMovement x y aim
+-- solutionDay2Part2 = multiplyDirections . sumDistancesConsideringAim . map (toSubMovement . words)
 
-sumDistancesConsideringAim :: [SubmarineMovement] -> SubmarineMovement
-sumDistancesConsideringAim = foldl sumDistancesConsideringAim' (MkSubmarineMovement 0 0 0)
+computeAim :: [SubMovementPlus] -> [SubMovementPlus]
+computeAim [] = []
+computeAim [MkSubMovementPlus direction magnitude _] = [MkSubMovementPlus direction magnitude 0]
+computeAim _ = []
+
+-- computeAim (MkSubMovement x y aim)
+--   | y > 0 = MkSubMovement x y (aim + 5)
+--   | y < 0 = MkSubMovement x y (aim - 3)
+--   | otherwise = MkSubMovement x y aim
+
+sumDistancesConsideringAim :: [SubMovement] -> SubMovement
+sumDistancesConsideringAim = foldr sumDistancesConsideringAim' (MkSubMovement 0 0)
   where
-    sumDistancesConsideringAim' firstMovement secondMovement = MkSubmarineMovement (xDirection firstMovement + xDirection secondMovement) (yDirection firstMovement + yDirection secondMovement) (aim firstMovement + aim secondMovement)
+    sumDistancesConsideringAim' firstMovement secondMovement = MkSubMovement (xDirection firstMovement + xDirection secondMovement) (yDirection firstMovement + yDirection secondMovement)
 
-sumDistances :: [SubmarineMovement] -> SubmarineMovement
-sumDistances = foldl sumDistances' (MkSubmarineMovement 0 0 0)
+sumDistances :: [SubMovement] -> SubMovement
+sumDistances = foldl sumDistances' (MkSubMovement 0 0)
   where
-    sumDistances' firstMovement secondMovement = MkSubmarineMovement (xDirection firstMovement + xDirection secondMovement) (yDirection firstMovement + yDirection secondMovement) 0
+    sumDistances' firstMovement secondMovement = MkSubMovement (xDirection firstMovement + xDirection secondMovement) (yDirection firstMovement + yDirection secondMovement)
 
-toDirections :: [String] -> SubmarineMovement
-toDirections directionAndDistance
-  | direction == "forward" = MkSubmarineMovement distance 0 0
-  | direction == "down" = MkSubmarineMovement 0 distance 0
-  | direction == "up" = MkSubmarineMovement 0 (- distance) 0
-  | otherwise = MkSubmarineMovement 0 0 0
+toSubMovement :: [String] -> SubMovement
+toSubMovement directionAndDistance
+  | direction == "forward" = MkSubMovement distance 0
+  | direction == "down" = MkSubMovement 0 distance
+  | direction == "up" = MkSubMovement 0 (- distance)
+  | otherwise = MkSubMovement 0 0
   where
     direction = head directionAndDistance
     distance = read (head (tail directionAndDistance))
+
+toSubMovementPlus :: [String] -> SubMovementPlus
+toSubMovementPlus directionAndMagnitude
+  | direction == "forward" = MkSubMovementPlus Forward magnitude 0
+  | direction == "down" = MkSubMovementPlus Down magnitude 0
+  | direction == "up" = MkSubMovementPlus Up magnitude 0
+  | otherwise = MkSubMovementPlus Forward 0 0
+  where
+    direction = head directionAndMagnitude
+    magnitude = read (head (tail directionAndMagnitude)) :: Int
 
 toInt :: String -> Int
 toInt = read
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,13 @@
 import Lib
-  ( SubmarineMovement (MkSubmarineMovement),
+  ( SubDirection (Down, Forward, Up),
+    SubMovement (MkSubMovement),
+    SubMovementPlus (MkSubMovementPlus),
     computeAim,
     solutionDay1Part1,
     solutionDay1Part2,
     solutionDay2Part1,
     solutionDay2Part2,
+    toSubMovementPlus,
   )
 import Test.Hspec (describe, hspec, it, shouldBe)
 
@@ -18,16 +21,27 @@ main = hspec $ do
   describe "day 2" $ do
     it "the solution to part one with sample data should be 150" $ do
       solutionDay2Part1 dayTwoSampleData `shouldBe` 150 -- full data result 1727835
-    it "the solution to part two with sample data should be x" $ do
-      solutionDay2Part2 dayTwoSampleData `shouldBe` 900 -- full data result ...
-  describe "computing the aim" $ do
-    it "given a forward direction should leave the aim unchanged" $ do
-      let movement = MkSubmarineMovement 5 0 0
-       in computeAim movement `shouldBe` movement
-    it "given a downward direction should increase the aim by 5" $ do
-      computeAim (MkSubmarineMovement 0 1 0) `shouldBe` MkSubmarineMovement 0 1 5
-    it "given an upward direction should decrease the aim by 3" $ do
-      computeAim (MkSubmarineMovement 0 (-5) 6) `shouldBe` MkSubmarineMovement 0 (-5) 3
+      -- it "the solution to part two with sample data should be x" $ do
+      --   solutionDay2Part2 dayTwoSampleData `shouldBe` 900 -- full data result ...
+  describe "parsing sub movements" $ do
+    it "given a forward direction and magnitude 5 should parse them" $ do
+      let input = ["forward", "5"]
+       in toSubMovementPlus input `shouldBe` MkSubMovementPlus Forward 5 0
+    it "given a downward direction and magnitude 1 should parse them" $ do
+      let input = ["down", "1"]
+       in toSubMovementPlus input `shouldBe` MkSubMovementPlus Down 1 0
+    it "given an upward direction and magnitude 7 should parse them" $ do
+      let input = ["up", "7"]
+       in toSubMovementPlus input `shouldBe` MkSubMovementPlus Up 7 0
+
+-- describe "computing the aim" $ do
+--   it "given a forward direction should leave the aim unchanged" $ do
+--     let movement = MkSubMovement 5 0
+--      in computeAim movement `shouldBe` movement
+--   it "given a downward direction should increase the aim by 5" $ do
+--     computeAim (MkSubMovement 0 1) `shouldBe` MkSubMovement 0 1
+--   it "given an upward direction should decrease the aim by 3" $ do
+--     computeAim (MkSubMovement 0 (-5)) `shouldBe` MkSubMovement 0 (-5)
 
 dayOneSampleData :: [String]
 dayOneSampleData = ["199", "200", "208", "210", "200", "207", "240", "269", "260", "263"]
