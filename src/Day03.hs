1cbd015b3e39c197e4d4d61a9de7b1322880013e
Adrian Schurz <adrian.schurz@check24.de>
2021-12-17 13:17:49 +0100

    Refactoring

--- src/Day03.hs
+++ src/Day03.hs
@@ -11,6 +11,23 @@ import Data.Tuple.Extra (both)
 solutionDay3Part1 :: [[Char]] -> Int
 solutionDay3Part1 input = (uncurry (*) . both toNumber . pairWithNegation . biggerThanHalfTheInputLength input . sumUpElementwise . toDiagnosticNumbers) input
 
+toDiagnosticNumbers :: [[Char]] -> [[Int]]
+toDiagnosticNumbers = map (map digitToInt)
+
+sumUpElementwise :: [[Int]] -> [Int]
+sumUpElementwise = foldr addElementwise (repeat 0)
+
+addElementwise :: [Int] -> [Int] -> [Int]
+addElementwise = zipWith (+)
+
+biggerThanHalfTheInputLength :: [String] -> [Int] -> [Bool]
+biggerThanHalfTheInputLength inputStrings = map ((>= halfTheInputLength) . fromIntegral)
+  where
+    halfTheInputLength = fromIntegral (length inputStrings) / 2
+
+pairWithNegation :: [Bool] -> ([Bool], [Bool])
+pairWithNegation input = (input, map not input)
+
 toNumber :: [Bool] -> Int
 toNumber = sum . zipWith (*) powersOfTwo . toZerosAndOnes . reverse
   where
@@ -18,28 +35,6 @@ toNumber = sum . zipWith (*) powersOfTwo . toZerosAndOnes . reverse
 
 toZerosAndOnes :: [Bool] -> [Int]
 toZerosAndOnes = map (\boolean -> if boolean then 1 else 0)
-
-pairWithNegation :: [Bool] -> ([Bool], [Bool])
-pairWithNegation input = (input, map not input)
-
-biggerThanHalfTheInputLength :: [String] -> [Int] -> [Bool]
-biggerThanHalfTheInputLength inputStrings = map ((>= halfTheInputLength) . fromIntegral)
-  where
-    halfTheInputLength = fromIntegral (length inputStrings) / 2 :: Float
-
-sumUpElementwise :: [[Int]] -> [Int]
-sumUpElementwise = foldr addElementwise (repeat 0)
-
-toDiagnosticNumbers :: [[Char]] -> [[Int]]
-toDiagnosticNumbers = map (map digitToInt)
-
-addElementwise :: [Int] -> [Int] -> [Int]
-addElementwise = zipWith (+)
-
--- solutionDay3Part1 = foldr justTheSecondOne ""
-
-justTheSecondOne :: p1 -> p2 -> p1
-justTheSecondOne a b = a
 
 solutionDay3Part2 :: [String] -> Int
 solutionDay3Part2 = const 0
