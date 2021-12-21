f9876b7ab4aa6b53a6f7cd139ae8b18ddda3788b
Adrian Schurz <adrian.schurz@check24.de>
2021-12-21 14:01:21 +0100

    Solve day 4 part 1

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,9 +1,12 @@
 module Day04
   ( solutionDay4Part1,
     solutionDay4Part2,
+    playBingo,
+    Marking (Marked, UnMarked),
   )
 where
 
+import Data.List (transpose)
 import Data.List.Split
   ( chunksOf,
     splitOn,
@@ -20,22 +23,64 @@ type BoardsInput = [String]
 
 type BoardRow = [String]
 
-type Board = [BoardRow]
+type Board = [[(String, Marking)]]
 
 data Marking = Marked | UnMarked deriving (Show, Eq)
 
-solutionDay4Part1 = prepareDrawnNumbers . prepareBoards . both removeEmptyRows . breakAtEmptyLine
+solutionDay4Part1 :: PuzzleInput -> Int
+solutionDay4Part1 = uncurry (*) . sumUnMarked . playBingo . prepareDrawnNumbers . prepareBoards . both removeEmptyRows . breakAtEmptyLine
 
-playBingo :: (DrawnNumbers, [[[(String, Marking)]]]) -> (Int, Board)
-playBingo = const (0, [])
+-- TODO parse numbers early
+
+sumUnMarked :: Maybe (Int, Board) -> (Int, Int)
+sumUnMarked Nothing = (0, 0)
+sumUnMarked (Just (lastDraw, winningBoard)) = (lastDraw, (sum . map derp . filter unMarked . concat) winningBoard)
+
+derp :: (String, Marking) -> Int
+derp (numberString, marking) = read numberString
+
+unMarked :: (String, Marking) -> Bool
+unMarked (_, marking) = marking == UnMarked
+
+playBingo :: (DrawnNumbers, [Board]) -> Maybe (Int, Board)
+playBingo numbersAndBoards
+  | ([], _) <- numbersAndBoards = Nothing
+  | (_, []) <- numbersAndBoards = Nothing
+  | otherwise = Just (playBingo' numbers boards)
+  where
+    numbers = fst numbersAndBoards
+    boards = snd numbersAndBoards
+
+playBingo' :: [String] -> [Board] -> (Int, Board)
+playBingo' numberStrings boards
+  | any complete boardsAfterMarking = (number, head (filter complete boardsAfterMarking))
+  | otherwise = playBingo' (tail numberStrings) boardsAfterMarking
+  where
+    number = (read . head) numberStrings
+    boardsAfterMarking = map (map (map (mark (head numberStrings)))) boards
+
+mark :: String -> (String, Marking) -> (String, Marking)
+mark drawnNumber bingoCell
+  | drawnNumber == fst bingoCell = (fst bingoCell, Marked)
+  | otherwise = bingoCell
+
+complete :: Board -> Bool
+complete board = any rowComplete board || any rowComplete (transpose board)
+
+rowComplete :: [(a, Marking)] -> Bool
+rowComplete = all isMarked
+
+isMarked :: (a, Marking) -> Bool
+isMarked (_, Marked) = True
+isMarked (_, UnMarked) = False
 
 boardScore :: (Int, Board) -> Int
 boardScore = const 0
 
-prepareBoards :: (DrawnNumbers, BoardsInput) -> (DrawnNumbers, [[[(String, Marking)]]])
+prepareBoards :: (DrawnNumbers, BoardsInput) -> (DrawnNumbers, [Board])
 prepareBoards = fmap (chunksOf 5 . map (map (,UnMarked) . words))
 
-prepareDrawnNumbers :: (DrawnNumbers, [[[(String, Marking)]]]) -> (DrawnNumbers, [[[(String, Marking)]]])
+prepareDrawnNumbers :: (DrawnNumbers, [Board]) -> (DrawnNumbers, [Board])
 prepareDrawnNumbers (numbers, boards) = (concatMap (splitOn ",") numbers, boards)
 
 breakAtEmptyLine :: PuzzleInput -> (DrawnNumbers, BoardsInput)
--- test/Day04Spec.hs
+++ test/Day04Spec.hs
@@ -1,7 +1,9 @@
 module Day04Spec (spec) where
 
 import Day04
-  ( solutionDay4Part1,
+  ( Marking (Marked, UnMarked),
+    playBingo,
+    solutionDay4Part1,
     solutionDay4Part2,
   )
 import Test.Hspec
@@ -15,12 +17,22 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "playing bingo" $ do
+    context "with neither numbers nor boards" $
+      it "should result in Nothing" $ do
+        playBingo ([], []) `shouldBe` Nothing
+    context "with no numbers but valid boards" $
+      it "should result in Nothing" $ do
+        playBingo ([], [[[("", UnMarked)], [("", UnMarked)]]]) `shouldBe` Nothing
+    context "with a number which completes the first board" $
+      it "should return the current number and the board" $ do
+        let almostCompleteBoard = [[("1", UnMarked)], [("2", Marked)]]
+            completeBoard = [[("1", Marked)], [("2", Marked)]]
+         in playBingo (["1"], [almostCompleteBoard]) `shouldBe` Just (1, completeBoard)
     context "with sample data for part 1" $
       it "should be 4512" $ do
         sampleData <- lines <$> readFile "puzzle-inputs/day-04-sample"
-        solutionDay4Part1 sampleData `shouldBe` ([], [])
-
--- context "with actual data for part 1" $
---   it "should be ..." $ do
---     actualData <- lines <$> readFile "puzzle-inputs/day-04"
---     solutionDay4Part1 actualData `shouldBe` 0
+        solutionDay4Part1 sampleData `shouldBe` 4512
+    context "with actual data for part 1" $
+      it "should be 50008" $ do
+        actualData <- lines <$> readFile "puzzle-inputs/day-04"
+        solutionDay4Part1 actualData `shouldBe` 50008
