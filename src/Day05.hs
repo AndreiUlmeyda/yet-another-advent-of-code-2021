794bcd7abf994c4c2c13abefcc4308f12395be81
Adrian Schurz <adrian.schurz@check24.de>
2021-12-22 21:52:41 +0100

    Towards a solution to day 5 part 1

--- src/Day05.hs
+++ src/Day05.hs
@@ -4,10 +4,53 @@ module Day05
   )
 where
 
+import Data.List.Split (splitOn)
 import Day04 (PuzzleInput)
 
-solutionDay5Part1 :: PuzzleInput -> Int
-solutionDay5Part1 = const 0
+data Line = MkLine
+  { startX :: Int,
+    startY :: Int,
+    endX :: Int,
+    endY :: Int
+  }
+  deriving (Show, Eq)
+
+type Coordinate = (Int, Int)
+
+--solutionDay5Part1 :: PuzzleInput -> [(String, String)]
+solutionDay5Part1 = concatMap listOfCoordinatesFrom . prepareLines
+
+listOfCoordinatesFrom :: Line -> [Coordinate]
+listOfCoordinatesFrom (MkLine sx sy ex ey)
+  | sx == ex = zip (fromSmallerToBigger sy ey) (repeat sx)
+  | sy == ey = zip (fromSmallerToBigger sx ex) (repeat sy)
+  | otherwise = error "tried to generate list of coordinates from diagonal line"
+  where
+    fromSmallerToBigger a b = [(min a b) .. (max a b)]
+
+prepareLines :: PuzzleInput -> [Line]
+prepareLines = filter notDiagonal . map (toLine . map read . concatMap (splitOn ",") . splitOn " -> ")
+
+notDiagonal :: Line -> Bool
+notDiagonal (MkLine sx sy ex ey) = sx == ex || sy == ey
+
+toLine :: [Int] -> Line
+toLine coordinates
+  | [sx, sy, ex, ey] <- coordinates = MkLine sx sy ex ey
+  | otherwise = error "couldn't parse line coordinates with more or less than 4 elements"
 
 solutionDay5Part2 :: PuzzleInput -> Int
 solutionDay5Part2 = const 0
+
+-- (0,0) -> non-diagonal if x = const or y = const
+
+-- 0,9 -> 5,9
+-- 8,0 -> 0,8 drop
+-- 9,4 -> 3,4
+-- 2,2 -> 2,1
+-- 7,0 -> 7,4
+-- 6,4 -> 2,0
+-- 0,9 -> 2,9
+-- 3,4 -> 1,4
+-- 0,0 -> 8,8
+-- 5,5 -> 8,2
--- test/Day05Spec.hs
+++ test/Day05Spec.hs
@@ -16,18 +16,19 @@ spec :: Spec
 spec = do
   describe "avoiding hydrothermal vents" $ do
     context "with sample data for part 1" $
-      it "should result in 5" $ do
-        sampleData <- lines <$> readFile "puzzle-inputs/day-05-sample"
-        solutionDay5Part1 sampleData `shouldBe` 0
-    context "with actual data for part 1" $
-      it "should result in xxx" $ do
-        actualData <- lines <$> readFile "puzzle-inputs/day-05"
-        solutionDay5Part1 actualData `shouldBe` 0
-    context "with sample data for part 2" $
       it "should result in xxx" $ do
         sampleData <- lines <$> readFile "puzzle-inputs/day-05-sample"
-        solutionDay5Part2 sampleData `shouldBe` 0
-    context "with actual data for part 2" $
-      it "should result in xxx" $ do
-        actualData <- lines <$> readFile "puzzle-inputs/day-05"
-        solutionDay5Part2 actualData `shouldBe` 0
+        solutionDay5Part1 sampleData `shouldBe` [(0, 9), (1, 9), (2, 9), (3, 9), (4, 9), (5, 9), (3, 4), (4, 4), (5, 4), (6, 4), (7, 4), (8, 4), (9, 4), (1, 2), (2, 2), (0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (0, 9), (1, 9), (2, 9), (1, 4), (2, 4), (3, 4)]
+
+-- context "with actual data for part 1" $
+--   it "should result in xxx" $ do
+--     actualData <- lines <$> readFile "puzzle-inputs/day-05"
+--     solutionDay5Part1 actualData `shouldBe` 0
+-- context "with sample data for part 2" $
+--   it "should result in xxx" $ do
+--     sampleData <- lines <$> readFile "puzzle-inputs/day-05-sample"
+--     solutionDay5Part2 sampleData `shouldBe` 0
+-- context "with actual data for part 2" $
+--   it "should result in xxx" $ do
+--     actualData <- lines <$> readFile "puzzle-inputs/day-05"
+--     solutionDay5Part2 actualData `shouldBe` 0
