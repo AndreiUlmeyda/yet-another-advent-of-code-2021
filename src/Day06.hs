fc7c4545df7bdfbac654f30f5a7356ed6e7d6f31
Adrian Schurz <adrian.schurz@check24.de>
2021-12-23 13:05:43 +0100

    Solve day 6 part 1

--- src/Day06.hs
+++ src/Day06.hs
@@ -4,10 +4,16 @@ module Day06
   )
 where
 
+import Data.List.Split (splitOn)
 import Day04 (PuzzleInput)
 
 solutionDay6Part1 :: PuzzleInput -> Int
-solutionDay6Part1 = const 0
+solutionDay6Part1 = length . (!! 80) . iterate (concatMap ageAndMultiply) . map read . splitOn "," . head
+
+ageAndMultiply :: Int -> [Int]
+ageAndMultiply age
+  | age == 0 = [6, 8]
+  | otherwise = [age -1]
 
 solutionDay6Part2 :: PuzzleInput -> Int
 solutionDay6Part2 = const 0
--- test/Day06Spec.hs
+++ test/Day06Spec.hs
@@ -1,5 +1,6 @@
 module Day06Spec (spec) where
 
+import Data.List (sort)
 import Day06
   ( solutionDay6Part1,
     solutionDay6Part2,
@@ -16,10 +17,10 @@ spec :: Spec
 spec = do
   describe "" $ do
     context "with sample data for part 1" $
-      it "should result in xxx" $ do
+      it "should result in 5934" $ do
         sampleData <- lines <$> readFile "puzzle-inputs/day-06-sample"
-        solutionDay6Part1 sampleData `shouldBe` 0
+        solutionDay6Part1 sampleData `shouldBe` 5934
     context "with actual data for part 1" $
-      it "should result in xxx" $ do
+      it "should result in 393019" $ do
         actualData <- lines <$> readFile "puzzle-inputs/day-06"
-        solutionDay6Part1 actualData `shouldBe` 0
+        solutionDay6Part1 actualData `shouldBe` 393019
