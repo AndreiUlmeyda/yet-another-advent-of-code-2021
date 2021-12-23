23f64f601be371f9c3304c90bff9a6abcad50252
Adrian Schurz <adrian.schurz@check24.de>
2021-12-23 19:52:05 +0100

    Somewhat simplify day 6 part 2

--- src/Day06.hs
+++ src/Day06.hs
@@ -5,6 +5,7 @@ module Day06
   )
 where
 
+import Control.Lens (element, set, (.~))
 import Data.List (group, sort)
 import Data.List.Split (splitOn)
 import Day04 (PuzzleInput)
@@ -37,38 +38,28 @@ ageAndMultiply age
   | otherwise = [age -1]
 
 solutionDay6Part2 :: PuzzleInput -> Int
-solutionDay6Part2 = sumPopulation . (!! 256) . iterate ageAndMultiplyImproved . toFishPopulation . map (\a -> (head a, length a)) . group . sort . prepareInput
+solutionDay6Part2 = sum . map snd . (!! 256) . iterate ageAndMultiplyImproved . supplyMissingIndices zeroCounts . map toElementAndLength . group . sort . prepareInput
 
-sumPopulation :: FishPopulation -> Int
-sumPopulation population = age0 population + age1 population + age2 population + age3 population + age4 population + age5 population + age6 population + age7 population + age8 population
+supplyMissingIndices :: [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
+supplyMissingIndices defaults [] = defaults
+supplyMissingIndices defaults [tuple] = set (element (fst tuple)) tuple defaults
+supplyMissingIndices defaults (tuple : rest) = supplyMissingIndices (set (element (fst tuple)) tuple defaults) rest
 
-ageAndMultiplyImproved :: FishPopulation -> FishPopulation
-ageAndMultiplyImproved population =
-  MkFishPopulation
-    { age0 = age1 population,
-      age1 = age2 population,
-      age2 = age3 population,
-      age3 = age4 population,
-      age4 = age5 population,
-      age5 = age6 population,
-      age6 = age7 population + age0 population,
-      age7 = age8 population,
-      age8 = age0 population
-    }
+zeroCounts :: [(Int, Int)]
+zeroCounts = zip [0 .. 8] (repeat 0)
 
-toFishPopulation :: [(Int, Int)] -> FishPopulation
-toFishPopulation = foldr derp (MkFishPopulation 0 0 0 0 0 0 0 0 0)
+toElementAndLength :: [a] -> (a, Int)
+toElementAndLength a = (head a, length a)
 
-derp :: (Int, Int) -> FishPopulation -> FishPopulation
-derp ageAndCount population
-  | fst ageAndCount == 0 = population {age0 = snd ageAndCount}
-  | fst ageAndCount == 1 = population {age1 = snd ageAndCount}
-  | fst ageAndCount == 2 = population {age2 = snd ageAndCount}
-  | fst ageAndCount == 3 = population {age3 = snd ageAndCount}
-  | fst ageAndCount == 4 = population {age4 = snd ageAndCount}
-  | fst ageAndCount == 5 = population {age5 = snd ageAndCount}
-  | fst ageAndCount == 6 = population {age6 = snd ageAndCount}
-  | fst ageAndCount == 7 = population {age7 = snd ageAndCount}
-  | otherwise = population {age8 = fst ageAndCount}
-
--- TODO clean up, drop the intermediate FishPopulation type, play around with lens
+ageAndMultiplyImproved :: [(Int, Int)] -> [(Int, Int)]
+ageAndMultiplyImproved agesAndCount =
+  [ (0, snd (agesAndCount !! 1)),
+    (1, snd (agesAndCount !! 2)),
+    (2, snd (agesAndCount !! 3)),
+    (3, snd (agesAndCount !! 4)),
+    (4, snd (agesAndCount !! 5)),
+    (5, snd (agesAndCount !! 6)),
+    (6, snd (agesAndCount !! 7) + snd (head agesAndCount)),
+    (7, snd (agesAndCount !! 8)),
+    (8, snd (head agesAndCount))
+  ]
