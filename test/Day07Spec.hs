3fa82ffe6fc757cd627284b7e3a75d6d18cf0a95
Adrian Schurz <adrian.schurz@check24.de>
2021-12-24 10:01:08 +0100

    Fix tests

--- test/Day07Spec.hs
+++ test/Day07Spec.hs
@@ -28,6 +28,6 @@ spec = do
         sampleData <- lines <$> readFile "puzzle-inputs/day-07-sample"
         solutionDay7Part2 sampleData `shouldBe` 168
     context "with actual data for part 2" $
-      it "should result in xxx" $ do
+      it "should result in 93214037" $ do
         actualData <- lines <$> readFile "puzzle-inputs/day-07"
-        solutionDay7Part2 actualData `shouldBe` 341558
+        solutionDay7Part2 actualData `shouldBe` 93214037
