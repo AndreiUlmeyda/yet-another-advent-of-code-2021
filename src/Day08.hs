94da312ec85338884236de50a1b0529a684f49ce
Adrian Schurz <adrian.schurz@check24.de>
2021-12-27 14:06:05 +0100

    WIP refactor day 8 part 2 to improve clarity

--- src/Day08.hs
+++ src/Day08.hs
@@ -46,17 +46,30 @@ pairWithMappings signalPatternsAndOutput = (inferMappingFrom signalPatterns, out
     output = signalPatternsAndOutput !! 1
 
 inferMappingFrom :: [SevenSegmentDigit] -> [SevenSegmentDigit]
-inferMappingFrom signals = (inferNine signals . inferSixSegmentDigits signals . inferFiveSegmentDigits signals . inferDigitsWithUniqueSegmentCount signals) emptyMapping
+inferMappingFrom signals =
+  ( inferNine signals
+      . inferSixSegmentDigits signals
+      . inferFiveSegmentDigits signals
+      . infer 2 twoSegmentsInCommonWith4
+      . infer 8 (isOfUniqueLength 7)
+      . infer 7 (isOfUniqueLength 3)
+      . infer 4 (isOfUniqueLength 4)
+      . infer 1 (isOfUniqueLength 2)
+  )
+    emptyMapping
   where
+    infer number inferenceStrategy mapping = set (element number) (inferenceStrategy mapping) mapping
+    isOfUniqueLength targetLength _ = fromJust (find (hasLength targetLength) signals)
     emptyMapping = replicate 10 ""
+    signalsOfLength5 = filter (hasLength 5) signals
+    twoSegmentsInCommonWith4 mapping = fromJust (find (\signal -> 2 == length (map (intersect signal) mapping !! 4)) signalsOfLength5)
 
 inferNine :: [SevenSegmentDigit] -> [SevenSegmentDigit] -> [SevenSegmentDigit]
 inferNine signals mapping = set (element 9) (head (signals \\ mapping)) mapping
 
 inferFiveSegmentDigits :: [SevenSegmentDigit] -> [SevenSegmentDigit] -> [SevenSegmentDigit]
-inferFiveSegmentDigits signals mapping = (find5 . find3 . find2) mapping
+inferFiveSegmentDigits signals mapping = (find5 . find3) mapping
   where
-    find2 = set (element 2) two
     find3 = set (element 3) three
     find5 = set (element 5) five
     two = fromJust $ find twoSegmentsInCommonWith4 signalsOfLength6
@@ -78,19 +91,8 @@ inferSixSegmentDigits signals mapping = (find0 . find6) mapping
     oneSegmentInCommonWith1 relevantSignals = 1 == length (map (intersect relevantSignals) mapping !! 1)
     fourSegmentsInCommonWith5 relevantSignals = 4 == length (map (intersect relevantSignals) mapping !! 5)
 
-inferDigitsWithUniqueSegmentCount :: [SevenSegmentDigit] -> [SevenSegmentDigit] -> [SevenSegmentDigit]
-inferDigitsWithUniqueSegmentCount signals = find8 . find7 . find4 . find1
-  where
-    find1 = substituteElementWithSignalOfLength 1 2 signals
-    find4 = substituteElementWithSignalOfLength 4 4 signals
-    find7 = substituteElementWithSignalOfLength 7 3 signals
-    find8 = substituteElementWithSignalOfLength 8 7 signals
-
 hasLength :: Foldable t => Int -> t a -> Bool
 hasLength n list = length list == n
-
-substituteElementWithSignalOfLength :: Int -> Int -> [SevenSegmentDigit] -> [SevenSegmentDigit] -> [SevenSegmentDigit]
-substituteElementWithSignalOfLength elementIndex targetLength signals = set (element elementIndex) (fromJust (find (hasLength targetLength) signals))
 
 prepareInput2 :: PuzzleInput -> [[[String]]]
 prepareInput2 = map (map (map sort . words) . splitOn " | ")
