c8319ce8fd653a294907ad537961fbf540ac177b
Adrian Schurz <adrian.schurz@check24.de>
2021-12-27 17:02:24 +0100

    Prepare day 9

---
+++ src/Day09.hs
@@ -0,0 +1,9 @@
+module Day09
+  ( solutionDay9Part1,
+    solutionDay9Part2,
+  )
+where
+
+solutionDay9Part1 = const 0
+
+solutionDay9Part2 = const 0
---
+++ test/Day09Spec.hs
@@ -0,0 +1,38 @@
+module Day09Spec (spec) where
+
+import Day09
+  ( solutionDay9Part1,
+    solutionDay9Part2,
+  )
+import Test.Hspec
+  ( Spec,
+    context,
+    describe,
+    it,
+    shouldBe,
+  )
+
+spec :: Spec
+spec = do
+  describe "" $ do
+    context "with sample data for part 1" $
+      it "should result in xxx" $ do
+        sampleData <- lines <$> readFile "puzzle-inputs/day-08-sample"
+        solutionDay9Part1 sampleData `shouldBe` 0
+
+-- context "with actual data for part 1" $
+--   it "should result in 355" $ do
+--     actualData <- lines <$> readFile "puzzle-inputs/day-08"
+--     solutionDay8Part1 actualData `shouldBe` 355
+-- context "with sample data for part 2" $
+--   it "should result in 61229" $ do
+--     sampleData <- lines <$> readFile "puzzle-inputs/day-08-sample"
+--     solutionDay8Part2 sampleData `shouldBe` 61229
+-- context "with different sample data for part 2" $
+--   it "should result in 9781" $ do
+--     sampleData <- lines <$> readFile "puzzle-inputs/day-08-smaller-sample"
+--     solutionDay8Part2 sampleData `shouldBe` 9781
+-- context "with actual data for part 2" $
+--   it "should result in 983030" $ do
+--     actualData <- lines <$> readFile "puzzle-inputs/day-08"
+--     solutionDay8Part2 actualData `shouldBe` 983030
