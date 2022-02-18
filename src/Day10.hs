833726b117c3a8771c73742fcbbb4871ef21622e
Adrian Schurz <adrian.schurz@check24.de>
2022-02-18 15:19:05 +0100

    WIP towards solving day 10 part 1

--- src/Day10.hs
+++ src/Day10.hs
@@ -2,11 +2,29 @@ module Day10
   ( solutionDay10Part1,
     solutionDay10Part2,
     firstCorruptCharacter,
+    parseChunks,
+    Chunk (MkChunk, MkInCompleteChunk),
+    contentOfFirstBracket,
+    upUntilNthCharacter,
   )
 where
 
-import Data.List (delete)
+import Data.List (delete, stripPrefix)
+import Data.Maybe (fromJust)
 import Day04 (PuzzleInput)
+import Prelude
+
+data Chunk
+  = MkChunk
+      { openingCharacter :: Char,
+        closingCharacter :: Char,
+        innerChunks :: [Chunk]
+      }
+  | MkInCompleteChunk
+      { openingCharacter :: Char,
+        innerChunks :: [Chunk]
+      }
+  deriving stock (Show, Eq)
 
 solutionDay10Part1 :: PuzzleInput -> [Maybe Char]
 solutionDay10Part1 = map firstCorruptCharacter
@@ -20,21 +38,83 @@ firstCorruptCharacter input
 removeMatchingBrackets :: String -> String
 removeMatchingBrackets [] = []
 removeMatchingBrackets (firstCharacter : rest)
-  | matchingCharacter firstCharacter `elem` rest = removeMatchingBrackets (matchingCharacterRemovedFrom rest)
+  | fromJust (matchingCharacter firstCharacter) `elem` rest =
+    if
+        | [] <- removeMatchingBrackets innerBracket -> removeMatchingBrackets (firstMatchingCharacterRemovedFrom rest)
+        | otherwise -> removeMatchingBrackets innerBracket ++ removeMatchingBrackets (firstMatchingCharacterRemovedFrom rest) -- the tricky case, there is a first character AND the inner brackets are not matched ->
   | otherwise = firstCharacter : removeMatchingBrackets rest
   where
-    matchingCharacterRemovedFrom = delete (matchingCharacter firstCharacter)
+    firstMatchingCharacterRemovedFrom = delete (fromJust (matchingCharacter firstCharacter))
+    innerBracket = takeWhile (/= fromJust (matchingCharacter firstCharacter)) rest -- the definition of inner bracket is wrong, if another bracket opens then the corresponding closing bracket is not the first but the second one
 
--- firstCorruptCharacter (firstCharacter : rest)
---   | rest <- [matchingCharacter firstCharacter] =
---   | otherwise = if Nothing == firstCorruptCharacter rest then Just firstCharacter else firstCorruptCharacter rest
+parseChunks :: String -> [Chunk]
+parseChunks stringOfBrackets
+  | [] <- stringOfBrackets = []
+  | [a] <- stringOfBrackets = [MkInCompleteChunk a []]
+  | (first : second : rest) <- stringOfBrackets,
+    second == fromJust (matchingCharacter first) =
+    MkChunk first second [] : parseChunks rest
+  | (first : second : rest) <- stringOfBrackets,
+    second /= fromJust (matchingCharacter first),
+    Nothing <- contentOfFirstBracket (second : rest) =
+    [MkInCompleteChunk first (parseChunks (second : rest))]
+  | (first : _ : _) <- stringOfBrackets, -- TODO in dire, dire need of a refactoring
+    Just bracketContent <- contentOfFirstBracket stringOfBrackets,
+    Just closingBracket <- matchingCharacter first,
+    Just partAfterClosingBracket <- stripPrefix bracketContent stringOfBrackets =
+    [MkChunk first closingBracket (parseChunks partAfterClosingBracket)]
+  | (first : _ : _) <- stringOfBrackets,
+    Just bracketContent <- contentOfFirstBracket stringOfBrackets,
+    Just closingBracket <- matchingCharacter first,
+    Nothing <- stripPrefix bracketContent stringOfBrackets =
+    [MkChunk first closingBracket (parseChunks bracketContent)]
+  | otherwise = error $ "unhandled case during parsing: " ++ stringOfBrackets -- TODO convert to a total function
 
-matchingCharacter :: Char -> Char
-matchingCharacter '(' = ')'
-matchingCharacter '[' = ']'
-matchingCharacter '{' = '}'
-matchingCharacter '<' = '>'
-matchingCharacter _ = ' '
+contentOfFirstBracket :: String -> Maybe String
+contentOfFirstBracket input
+  | "" <- input = Nothing
+  | [_] <- input = Nothing
+  | (first : rest) <- input = Just $ contentOfFirstBracket' first 0 rest -- react to the second character having a matching character
+
+contentOfFirstBracket' :: Char -> Int -> String -> String
+contentOfFirstBracket' _ _ [] = ""
+contentOfFirstBracket' openingBracket bracketBalance (first : rest)
+  | bracketBalance > 0 = upUntilNthCharacter bracketBalance closingBracket (first : rest)
+  | Nothing <- matchingCharacter openingBracket = first : contentOfFirstBracket' openingBracket bracketBalance rest
+  | first == openingBracket = first : contentOfFirstBracket' openingBracket (bracketBalance + 1) rest
+  | otherwise = takeWhile (/= closingBracket) (first : rest)
+  where
+    closingBracket = fromJust $ matchingCharacter openingBracket
+
+upUntilNthCharacter :: Int -> Char -> String -> String
+upUntilNthCharacter count character characters
+  | count == 0 = characters
+  | count == 1 = upUntilNextCharacter
+  | otherwise = upUntilNextCharacter ++ upUntilNthCharacter (count - 1) character (tail (dropWhile (/= character) characters))
+  where
+    upUntilNextCharacter = if character `elem` characters then a ++ [head b] else a
+    (a, b) = span (/= character) characters
+
+-- cb aea
+-- TODO type synonyms
+
+-- first two don't match -> need to recurse immediately
+-- possible terminations of recursion: (should include matched and unmatched cases)
+--     rest is empty
+-- tools:
+-- function to determine if there is a closing bracket before another opening bracket:
+
+matchingCharacter :: Char -> Maybe Char
+matchingCharacter '(' = Just ')'
+matchingCharacter '[' = Just ']'
+matchingCharacter '{' = Just '}'
+matchingCharacter '<' = Just '>'
+matchingCharacter _ = Nothing
 
 solutionDay10Part2 :: PuzzleInput -> Int
 solutionDay10Part2 = const 0
+
+-- TODO avoid unsafe functions entirely
+
+-- from a first and a rest, construct a chunk
+-- chunks can be
--- test/Day10Spec.hs
+++ test/Day10Spec.hs
@@ -2,8 +2,13 @@ module Day10Spec (spec) where
 
 import Day10
   ( -- solutionDay10Part2,
+
+    Chunk (MkChunk, MkInCompleteChunk),
+    contentOfFirstBracket,
     firstCorruptCharacter,
+    parseChunks,
     solutionDay10Part1,
+    upUntilNthCharacter,
   )
 import Test.Hspec
   ( Spec,
@@ -15,6 +20,66 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
+  describe "get contents of the first bracket" $ do
+    context "given the empty string" $
+      it "should result in Nothing" $ do
+        contentOfFirstBracket "" `shouldBe` Nothing
+    context "given a single opening bracket" $
+      it "should result in Nothing" $ do
+        contentOfFirstBracket "(" `shouldBe` Nothing
+    context "given two matching brackets" $
+      it "should result in the empty string" $ do
+        contentOfFirstBracket "()" `shouldBe` Just ""
+    context "given characters inbetween two matched brackets" $
+      it "should result in just those characters" $ do
+        contentOfFirstBracket "(something)" `shouldBe` Just "something"
+    context "given another opening bracket of the same type" $
+      it "should consider that bracket as being associated with the inner bracket" $ do
+        contentOfFirstBracket "((something)" `shouldBe` Just "(something)"
+    context "given another inner pair of matched brackets of the same type" $
+      it "should return the inner brackets" $ do
+        contentOfFirstBracket "(())" `shouldBe` Just "()"
+
+  describe "up until the nth character" $ do
+    context "given the empty string" $
+      it "should result in the empty string" $ do
+        upUntilNthCharacter 1 'a' "" `shouldBe` []
+    context "given a string without the relevant character" $
+      it "should result in the complete string" $ do
+        upUntilNthCharacter 1 'a' "bc" `shouldBe` "bc"
+    context "given the relevant character and count 1" $
+      it "should not contain the relevant character" $ do
+        upUntilNthCharacter 1 'a' "bcad" `shouldBe` "bca"
+    context "extracting up until the second occurence" $
+      it "should ignore the first one" $ do
+        upUntilNthCharacter 2 'a' "bcadaw" `shouldBe` "bcada"
+    context "extracting up until the third occurence" $
+      it "should should ignore the first two" $ do
+        upUntilNthCharacter 3 'a' "bcadawrap" `shouldBe` "bcadawra"
+    context "given a string containing that character" $
+      it "should include it" $ do
+        upUntilNthCharacter 1 ')' "()" `shouldBe` "()"
+
+  describe "parsing chunks" $ do
+    context "given the empty string" $
+      it "should result in an empty list" $ do
+        parseChunks "" `shouldBe` []
+    context "given a single opening bracket" $
+      it "should result in an incomplete chunk" $ do
+        parseChunks "(" `shouldBe` [MkInCompleteChunk '(' []]
+    context "given a matched bracket" $
+      it "should result in a complete chunk with correct characters" $ do
+        parseChunks "()" `shouldBe` [MkChunk '(' ')' []]
+    context "given two matched brackets in a row" $
+      it "should result in a list of two complete chunks with correct characters" $ do
+        parseChunks "()()" `shouldBe` [MkChunk '(' ')' [], MkChunk '(' ')' []]
+    context "given two nested brackets, each of them matched" $
+      it "should result in a chunk inside of the 'innerChunks' of the first" $ do
+        parseChunks "(<>)" `shouldBe` [MkChunk '(' ')' [MkChunk '<' '>' []]]
+    context "given " $
+      it "should result in " $ do
+        parseChunks "(<)" `shouldBe` [MkChunk '(' ')' [MkInCompleteChunk '<' []]]
+
   -- describe "" $ do
   --   context "with sample data for part 1" $
   --     it "should result in x" $ do
@@ -36,6 +101,13 @@ spec = do
     context "given nested pairs of matched brackets" $
       it "should result in Nothing" $ do
         firstCorruptCharacter "({<>})" `shouldBe` Nothing
+    context "given a matching bracket which is outside of the current chunk" $
+      it "should result an unmatched bracket" $ do
+        firstCorruptCharacter "(<)>" `shouldBe` Just '<'
+
+-- context "given a matching bracket which is outside of the current chunk" $
+--   it "should result an unmatched bracket" $ do
+--     firstCorruptCharacter "({}()<)>" `shouldBe` Just '<'
 
 -- context "with actual data for part 1" $
 --   it "should result in 518" $ do
