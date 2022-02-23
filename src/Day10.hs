7e7c7e9cdce2f65c21b01d41b6621abe97b89a3a
Adrian Schurz <adrian.schurz@check24.de>
2022-02-23 14:33:38 +0100

    Solve day 10 part 2

--- src/Day10.hs
+++ src/Day10.hs
@@ -5,19 +5,20 @@ module Day10
   )
 where
 
-import Data.Maybe (mapMaybe)
+import Data.List (sort)
+import Data.Maybe (isNothing, mapMaybe)
 import Day04 (PuzzleInput)
 import Prelude
 
 solutionDay10Part1 :: PuzzleInput -> Int
-solutionDay10Part1 = sum . map score . mapMaybe firstCorruptCharacter
+solutionDay10Part1 = sum . map scoreBracket . mapMaybe firstCorruptCharacter
 
-score :: Char -> Int
-score symbol
-  | ')' <- symbol = 3
-  | ']' <- symbol = 57
-  | '}' <- symbol = 1197
-  | '>' <- symbol = 25137
+scoreBracket :: Char -> Int
+scoreBracket bracket
+  | ')' <- bracket = 3
+  | ']' <- bracket = 57
+  | '}' <- bracket = 1197
+  | '>' <- bracket = 25137
   | otherwise = 0
 
 firstCorruptCharacter :: String -> Maybe Char
@@ -49,6 +50,44 @@ isClosedBy '<' '>' = True
 isClosedBy _ _ = False
 
 solutionDay10Part2 :: PuzzleInput -> Int
-solutionDay10Part2 = const 0
+solutionDay10Part2 = median . map (scoreLine . completeLine) . filter (isNothing . firstCorruptCharacter)
+
+completeLine :: String -> String
+completeLine = completeLine' []
+
+completeLine' :: String -> String -> String
+completeLine' openedBrackets [] = map complementaryBracket openedBrackets
+completeLine' openedBrackets (first : rest)
+  | isOpeningCharacter first = completeLine' (first : openedBrackets) rest
+  | isClosingCharacter first,
+    head openedBrackets `isClosedBy` first =
+    completeLine' (tail openedBrackets) rest
+  | otherwise = map complementaryBracket openedBrackets -- unify the first pattern with this one
+
+complementaryBracket :: Char -> Char
+complementaryBracket '(' = ')'
+complementaryBracket '{' = '}'
+complementaryBracket '[' = ']'
+complementaryBracket '<' = '>'
+complementaryBracket _ = error "derp"
+
+scoreLine :: String -> Int
+scoreLine = scoreLine' 0
+
+scoreLine' totalScore [] = totalScore
+scoreLine' totalScore (firstBracket : rest) = scoreLine' (totalScore * 5 + scoreBracketPartTwo firstBracket) rest
+
+scoreBracketPartTwo :: Char -> Int
+scoreBracketPartTwo bracket
+  | ')' <- bracket = 1
+  | ']' <- bracket = 2
+  | '}' <- bracket = 3
+  | '>' <- bracket = 4
+  | otherwise = 0
+
+median :: Ord a => [a] -> a
+median list = ((!! halfTheInputLength) . sort) list
+  where
+    halfTheInputLength = length list `div` 2
 
 -- TODO avoid unsafe functions entirely
--- test/Day10Spec.hs
+++ test/Day10Spec.hs
@@ -1,9 +1,9 @@
 module Day10Spec (spec) where
 
 import Day10
-  ( -- solutionDay10Part2,
-    firstCorruptCharacter,
+  ( firstCorruptCharacter,
     solutionDay10Part1,
+    solutionDay10Part2,
   )
 import Test.Hspec
   ( Spec,
@@ -24,6 +24,17 @@ spec = do
       it "should result in 311949" $ do
         actualData <- lines <$> readFile "puzzle-inputs/day-10"
         solutionDay10Part1 actualData `shouldBe` 311949
+
+  describe "completing, then scoring lines" $ do
+    context "with sample data for part 2" $
+      it "should result in x" $ do
+        sampleData <- lines <$> readFile "puzzle-inputs/day-10-sample"
+        solutionDay10Part2 sampleData
+          `shouldBe` 288957
+    context "with actual data for part 2" $
+      it "should result in x" $ do
+        actualData <- lines <$> readFile "puzzle-inputs/day-10"
+        solutionDay10Part2 actualData `shouldBe` 3042730309
 
   describe "finding the first corrupt character" $ do
     context "given the empty string" $
@@ -65,13 +76,3 @@ spec = do
   context "given sample line 5" $
     it "should result in >" $ do
       firstCorruptCharacter "<{([([[(<>()){}]>(<<{{" `shouldBe` Just '>'
-
--- context "with sample data for part 2" $
---   it "should result in 1134" $ do
---     sampleData <- lines <$> readFile "puzzle-inputs/day-09-sample"
---     solutionDay9Part2 sampleData
---       `shouldBe` 1134
--- context "with actual data for part 2" $
---   it "should result in 949905" $ do
---     actualData <- lines <$> readFile "puzzle-inputs/day-09"
---     solutionDay9Part2 actualData `shouldBe` 949905
