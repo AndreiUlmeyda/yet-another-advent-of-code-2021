9021ed3bafd49c35e3f6a1a2a010912cdfe4bfe2
Adrian Schurz <adrian.schurz@check24.de>
2022-03-20 09:58:18 +0100

    WIP towards solving day 11 part 1

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,16 +1,63 @@
 module Day11
   ( solutionDay11Part1,
     solutionDay11Part2,
+    increaseOctopusEnergies,
+    Octopus (MkOctopus),
   )
 where
 
+import Data.Array (Array, elems, listArray)
+import Data.Char (digitToInt)
+import Data.Maybe (fromJust, isJust)
 import Day04 (PuzzleInput)
 import Prelude
 
+data Octopus = MkOctopus
+  { energyLevel :: Int,
+    numberOfFlashes :: Int
+  }
+  deriving stock (Show, Eq)
+
+type OctopusArray = Array Int Octopus
+
+initialNumberOfFlashes :: Int
+initialNumberOfFlashes = 0
+
+parseOctopus :: Int -> Maybe Octopus
+parseOctopus initialEnergyLevel
+  | initialEnergyLevel >= 0,
+    initialEnergyLevel <= 9 =
+    Just $ MkOctopus initialEnergyLevel initialNumberOfFlashes
+  | otherwise = Nothing
+
+parseInput :: PuzzleInput -> OctopusArray
+parseInput puzzleInput
+  | all isJust resultingOctopusList = listArray (inputSizeX, inputSizeY) $ map fromJust resultingOctopusList
+  | otherwise = error "Error: The input needs to consist of digits only."
+  where
+    resultingOctopusList = map (parseOctopus . digitToInt) (concat puzzleInput)
+    inputSizeX = length puzzleInput
+    inputSizeY = length $ head puzzleInput
+
 -- ######### Part One #########
 solutionDay11Part1 :: PuzzleInput -> Int
-solutionDay11Part1 = const 0
+solutionDay11Part1 = countFlashes . (!! 100) . iterate (resolveFlashes . increaseOctopusEnergies) . parseInput
+
+increaseOctopusEnergies :: OctopusArray -> OctopusArray
+increaseOctopusEnergies = fmap increaseOctopusEnergy
+
+increaseOctopusEnergy :: Octopus -> Octopus
+increaseOctopusEnergy (MkOctopus energy flashCount) = MkOctopus (energy + 1) flashCount
+
+resolveFlashes :: OctopusArray -> OctopusArray
+resolveFlashes = id
+
+countFlashes :: OctopusArray -> Int
+countFlashes = sum . elems . fmap numberOfFlashes
 
 -- ######### Part Two #########
 solutionDay11Part2 :: PuzzleInput -> Int
 solutionDay11Part2 = const 0
+
+-- instead of increasing + resetting each step I probably need to
+-- first increase, then resolve flashes
--- test/Day11Spec.hs
+++ test/Day11Spec.hs
@@ -1,7 +1,10 @@
 module Day11Spec (spec) where
 
+import Data.Array (listArray)
 import Day11
-  ( solutionDay11Part1,
+  ( Octopus (MkOctopus),
+    increaseOctopusEnergies,
+    solutionDay11Part1,
     solutionDay11Part2,
   )
 import Test.Hspec
@@ -14,15 +17,15 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "" $ do
-    context "with sample data for part 1" $
-      it "should result in " $ do
-        sampleData <- lines <$> readFile "puzzle-inputs/day-11-sample"
-        solutionDay11Part1 sampleData `shouldBe` 0
-    context "with actual data for part 1" $
-      it "should result in " $ do
-        actualData <- lines <$> readFile "puzzle-inputs/day-11"
-        solutionDay11Part1 actualData `shouldBe` 0
+  -- describe "" $ do
+  --   context "with sample data for part 1" $
+  --     it "should result in " $ do
+  --       sampleData <- lines <$> readFile "puzzle-inputs/day-11-sample"
+  --       solutionDay11Part1 sampleData `shouldBe` []
+  --   context "with actual data for part 1" $
+  --     it "should result in " $ do
+  --       actualData <- lines <$> readFile "puzzle-inputs/day-11"
+  --       solutionDay11Part1 actualData `shouldBe` []
 
   describe "" $ do
     context "with sample data for part 2" $
@@ -34,3 +37,12 @@ spec = do
       it "should result in " $ do
         actualData <- lines <$> readFile "puzzle-inputs/day-10"
         solutionDay11Part2 actualData `shouldBe` 0
+
+  describe "counting octopus flashes" $ do
+    context "a single octopus" $
+      it "should just increment its value" $ do
+        increaseOctopusEnergies (listArray (1, 1) [MkOctopus 0 0]) `shouldBe` listArray (1, 1) [MkOctopus 1 0]
+
+-- context "a single octopus with value 9" $
+--   it "should reset its value to zero and increment the flash counter" $ do
+--     increaseOctopusEnergies (listArray (1, 1) [MkOctopus 9 0]) `shouldBe` listArray (1, 1) [MkOctopus 0 1]
