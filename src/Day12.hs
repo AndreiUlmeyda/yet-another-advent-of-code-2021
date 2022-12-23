e91bd59cfe4e6147c89b83f52decbd141b6dc074
Adrian Schurz <adrian.schurz@check24.de>
2022-12-23 10:56:53 +0100

    WIP towards day 12

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,15 +1,63 @@
 module Day12
   ( solutionDay12Part1,
     solutionDay12Part2,
+    generatePaths,
+    Connection (MkConnection),
+    mkConnection,
   )
 where
 
+import Data.Char (isUpper)
+import Data.List ((\\))
+import Data.List.Split
+  ( splitOn,
+  )
 import Day04 (PuzzleInput)
 import Prelude
 
+data CaveSize = SmallCave | BigCave deriving stock (Show, Eq)
+
+type Path = [Connection]
+
+data Connection = MkConnection
+  { caveOne :: String,
+    caveTwo :: String,
+    caveOneSize :: CaveSize,
+    caveTwoSize :: CaveSize,
+    wasVisited :: Bool
+  }
+  deriving stock (Show, Eq)
+
+mkCaveSize :: String -> CaveSize
+mkCaveSize caveName
+  | length caveName == 1 && isUpper (head caveName) = BigCave
+  | otherwise = SmallCave
+
+mkConnection :: [String] -> Connection
+mkConnection startAndEnd = MkConnection start end (mkCaveSize start) (mkCaveSize end) False
+  where
+    start = head startAndEnd
+    end = head $ tail startAndEnd
+
 -- ######### Part One #########
-solutionDay12Part1 :: PuzzleInput -> Int
-solutionDay12Part1 = const 1
+solutionDay12Part1 :: PuzzleInput -> [[Connection]]
+solutionDay12Part1 = generatePaths . parseCaveConnections
+
+generatePaths :: [Connection] -> [Path]
+generatePaths connections
+  | ((== 1) . length) (filter ((== "start") . caveOne) connections) = error ""
+  | otherwise = []
+
+generatePaths' :: [Connection] -> [Path]
+generatePaths' [] = []
+generatePaths' (connection : rest) = path : generatePaths unusedPaths
+  where
+    path = connection : rest
+    unusedPaths = rest \\ path
+    connectedToFirst = id
+
+parseCaveConnections :: [String] -> [Connection]
+parseCaveConnections = fmap (mkConnection . splitOn "-")
 
 -- ######### Part Two #########
 solutionDay12Part2 :: PuzzleInput -> Int
--- test/Day12Spec.hs
+++ test/Day12Spec.hs
@@ -1,7 +1,10 @@
 module Day12Spec (spec) where
 
 import Day12
-  ( solutionDay12Part1,
+  ( Connection (MkConnection),
+    generatePaths,
+    mkConnection,
+    solutionDay12Part1,
     solutionDay12Part2,
   )
 import Test.Hspec
@@ -14,11 +17,29 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "" $ do
-    context "with sample data for part 1" $
-      it "should result in x" $ do
-        sampleData <- lines <$> readFile "puzzle-inputs/day-12-sample"
-        solutionDay12Part1 sampleData `shouldBe` 1
+  -- describe "counting paths" $ do
+    -- context "with sample data for part 1" $
+    --   it "should result in x" $ do
+    --     sampleData <- lines <$> readFile "puzzle-inputs/day-12-sample"
+    --     solutionDay12Part1 sampleData `shouldBe` []
+  describe "generating paths" $ do
+    context "given an empty list of connections" $
+      it "should result in an empty list of paths" $ do
+        generatePaths [] `shouldBe` []
+    -- context "given a single connection" $
+    --   it "should result a single path containing that connection" $ do
+    --     let simpleConnection = mkConnection ["start", "end"]
+    --     generatePaths [simpleConnection] `shouldBe` [[simpleConnection]]
+    -- context "given two connections with a shared cave" $
+    --   it "should result in one path containing the two connections" $ do
+    --     let connection1 = mkConnection ["start", "a"]
+    --     let connection2 = mkConnection ["a", "end"]
+    --     generatePaths [connection1, connection2] `shouldBe` [[connection1, connection2]]
+    -- context "given two connections without a common cave" $
+    --   it "should result in two paths of length one" $ do
+    --     let connection1 = mkConnection ["start", "a"]
+    --     let connection2 = mkConnection ["b", "end"]
+    --     generatePaths [connection1, connection2] `shouldBe` [[connection1], [connection2]]
 
 -- context "with actual data for part 1" $
 --   it "should result in x" $ do
